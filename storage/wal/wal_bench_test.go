74640133aff7f115ba6f08989a0e357006d78de3
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2022-11-04 16:35:40 -0400

    Tune WAL benchmarks to isolate bottleneck
    This commit updates the `storage/wal` benchmarks to avoid constructing entries
    inside the timed benchmarking region. This makes the benchmarking results more
    accurate.
    
    With this change, we see the following results:
    ```
    GOOS=linux GOARCH=amd64 go test -c -o walbench ./storage/wal
    
    export CLUSTER=$USER-raft
    roachprod create $CLUSTER --nodes=1 --clouds=aws --aws-machine-type=m6i.4xlarge --local-ssd=false --aws-ebs-volume-type=gp3 --aws-ebs-throughput=1000 --aws-ebs-iops=16000 --aws-ebs-volume-size=500
    roachprod put $CLUSTER walbench
    
    roachprod run $CLUSTER -- ./walbench --test.bench='Benchmark././bytes=4096' --test.benchtime=3s
    
    goos: linux
    goarch: amd64
    pkg: github.com/nvanbenschoten/rafttoy/storage/wal
    cpu: Intel(R) Xeon(R) Platinum 8375C CPU @ 2.90GHz
    BenchmarkEtcd/batch=1/bytes=4096-48         	    1516	   2361407 ns/op	   1.73 MB/s
    BenchmarkEtcd/batch=4/bytes=4096-48         	    1486	   2451668 ns/op	   6.68 MB/s
    BenchmarkEtcd/batch=16/bytes=4096-48        	    1416	   2695436 ns/op	  24.31 MB/s
    BenchmarkEtcd/batch=64/bytes=4096-48        	    1063	   3576743 ns/op	  73.29 MB/s
    BenchmarkEtcd/batch=256/bytes=4096-48       	     684	   5318841 ns/op	 197.14 MB/s
    BenchmarkEtcd/batch=1024/bytes=4096-48      	     372	   9664302 ns/op	 434.00 MB/s
    BenchmarkEtcd/batch=4096/bytes=4096-48      	     133	  27076374 ns/op	 619.63 MB/s
    BenchmarkEtcd/batch=16384/bytes=4096-48     	      40	  96801241 ns/op	 693.26 MB/s
    BenchmarkEtcd/batch=65536/bytes=4096-48     	      10	 365308246 ns/op	 734.82 MB/s
    BenchmarkEtcd/batch=262144/bytes=4096-48    	       2	1576949460 ns/op	 680.90 MB/s
    BenchmarkPebble/batch=1/bytes=4096-48       	    4398	    834052 ns/op	   4.91 MB/s
    BenchmarkPebble/batch=4/bytes=4096-48       	    3663	   1022605 ns/op	  16.02 MB/s
    BenchmarkPebble/batch=16/bytes=4096-48      	    3052	   1174156 ns/op	  55.82 MB/s
    BenchmarkPebble/batch=64/bytes=4096-48      	    1540	   2037412 ns/op	 128.67 MB/s
    BenchmarkPebble/batch=256/bytes=4096-48     	     508	   7522734 ns/op	 139.39 MB/s
    BenchmarkPebble/batch=1024/bytes=4096-48    	     122	  28596430 ns/op	 146.67 MB/s
    BenchmarkPebble/batch=4096/bytes=4096-48    	      36	  94935344 ns/op	 176.72 MB/s
    BenchmarkPebble/batch=16384/bytes=4096-48   	       9	 374232825 ns/op	 179.32 MB/s
    BenchmarkPebble/batch=65536/bytes=4096-48   	       2	1720566289 ns/op	 156.02 MB/s
    BenchmarkPebble/batch=262144/bytes=4096-48  	       1	7800188387 ns/op	 137.66 MB/s
    
    roachprod run $CLUSTER -- dd if=/dev/zero of=/mnt/data1/f.tmp bs=64M count=1000 oflag=dsync
    1000+0 records in
    1000+0 records out
    67108864000 bytes (67 GB, 62 GiB) copied, 62.8817 s, 1.1 GB/s
    ```
    
    Neither the etcd WAL or Pebble are able to saturate the disk. However, etcd's
    WAL gets much closer.
    
    We are seeing the effects of write amplification here, where Pebble performs
    additional work to index the Raft log by constructing an LSM. However, that
    doesn't entirely explain the throughput delta. dstat shows that the etcd WAL's
    optimal config (batch=16384) drives about 750 MB/s of disk writes. Meanwhile,
    Pebble's optimal config (batch=16384) drives about 400 MB/s of disk writes. So
    write amplification is between 2x and 3x in Pebble and it is failing to saturate
    the disk.

--- storage/wal/wal_bench_test.go
+++ storage/wal/wal_bench_test.go
@@ -13,25 +13,17 @@ import (
 )
 
 func benchmarkWalConfig(b *testing.B, batchSize, bytes int, w wal.Wal) {
-	workers := workload.NewWorkers(workload.Config{
+	props := b.N * batchSize
+	worker := workload.NewWorkers(workload.Config{
 		KeyPrefix: engine.MinDataKey,
 		KeyLen:    len(engine.MinDataKey) + 8,
 		ValLen:    bytes,
 		Workers:   1,
-		Proposals: b.N,
-	})
-	worker := workers[0]
+		Proposals: props,
+	})[0]
 
+	entries := make([]raftpb.Entry, 0, props)
 	var a util.ByteAllocator
-	entries := make([]raftpb.Entry, 0, batchSize)
-	appendEntries := func() {
-		if len(entries) > 0 {
-			w.Append(entries)
-			entries = entries[:0]
-		}
-	}
-
-	b.ResetTimer()
 	var index uint64
 	for prop := worker.NextProposal(); prop != nil; prop = worker.NextProposal() {
 		index++
@@ -42,26 +34,26 @@ func benchmarkWalConfig(b *testing.B, batchSize, bytes int, w wal.Wal) {
 		// prop is only valid until NextProposal is called again, so copy it.
 		a, e.Data = a.Copy(prop)
 		entries = append(entries, e)
-		if len(entries) == batchSize {
-			appendEntries()
-		}
 	}
-	// Flush out the remaining partial batch.
-	appendEntries()
 
-	b.StopTimer()
-	b.SetBytes(int64(bytes))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		batch := entries[i*batchSize : (i+1)*batchSize]
+		w.Append(batch)
+	}
+	b.SetBytes(int64(bytes * batchSize))
 }
 
 func benchmarkWal(b *testing.B, walFn func(root string) wal.Wal) {
-	util.RunFor(b, "batch", 1, 3, 4, func(b *testing.B, batchSize int) {
-		util.RunFor(b, "bytes", 1, 3, 5, func(b *testing.B, bytes int) {
-			dir, err := ioutil.TempDir(``, ``)
+	util.RunFor(b, "batch", 1, 2, 10, func(b *testing.B, batchSize int) {
+		util.RunFor(b, "bytes", 1, 3, 6, func(b *testing.B, bytes int) {
+			dir, err := ioutil.TempDir("/mnt/data1", "")
 			if err != nil {
 				b.Fatal(err)
 			}
 			defer os.RemoveAll(dir)
 			w := walFn(dir)
+			defer w.CloseWal()
 			benchmarkWalConfig(b, batchSize, bytes, w)
 		})
 	})
